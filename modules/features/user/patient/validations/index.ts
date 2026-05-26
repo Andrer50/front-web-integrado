33f45a2787d67fd5f416f387079f35cecb9dfdba
vero-martinez <veris.14@hotmail.com>
2026-05-25 19:10:43 -0500

    separar validacion de crear y editar paciente

--- modules/features/user/patient/validations/index.ts
+++ modules/features/user/patient/validations/index.ts
@@ -22,3 +22,23 @@ export const patientValidationSchema = Yup.object({
   gender: Yup.string().required("Requerido"),
   address: Yup.string().optional(),
 });
+
+export const patientUpdateValidationSchema = Yup.object({
+  firstName: Yup.string()
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Solo se permiten letras")
+    .required("Requerido"),
+  lastName: Yup.string()
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Solo se permiten letras")
+    .required("Requerido"),
+  email: Yup.string().email("Email inválido").required("Requerido"),
+  documentNumber: Yup.string()
+    .matches(/^[0-9]{8}$/, "DNI debe tener 8 dígitos")
+    .required("Requerido"),
+  phone: Yup.string()
+    .matches(/^[0-9]+$/, "Solo se permiten números")
+    .length(9, "El teléfono debe tener 9 dígitos")
+    .required("Requerido"),
+  birthDate: Yup.string().required("Requerido"),
+  gender: Yup.string().required("Requerido"),
+  address: Yup.string().optional(),
+});
